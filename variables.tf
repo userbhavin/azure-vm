fbffecc7e761a40bd3b1023cbc1686d6d98c50b8
userbhavin <58483969+userbhavin@users.noreply.github.com>
2021-10-06 18:00:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,19 +23,19 @@ variable "location" {
 }
 
 variable "client_id" {
-  description = "client_id"
+  description = "4580cb14-b9d9-4917-9f8c-c814e5f1f65a"
 }
 
 variable "client_secret" {
-  description = "client_secret"
+  description = "nK_mDgo-HU5U~qVp_.ynKQgBvHdPVabMC4"
 }
 
 variable "subscription_id" {
-  description = "client_id"
+  description = "4bd67a8c-b219-4e2c-b4c1-fac8710191e6"
 }
 
 variable "tenant_id" {
-  description = "tenant_id"
+  description = "d2a4b3be-2406-46d8-b098-17361e38b3c6"
 }
 
 
